41361c101369a83997dbe5436d440a4416eca3b9
Evan A Martin <evanamartin@gmail.com>
2017-07-11 15:08:17 -0700

    remove nonhuman genes

--- R/cleanGenes.R
+++ R/cleanGenes.R
@@ -26,34 +26,38 @@
 #' @import tibble
 #'
 #' @importFrom AnnotationDbi as.data.frame
+#' @importFrom purrr map_chr map_int map_lgl
 #'
 cleanGenes <- function (geneList) {
 
-  value <- gene_symbol <- NULL
+  value <- geneSymbol <- NULL
 
   symbolDF <- AnnotationDbi::as.data.frame (org.Hs.egSYMBOL)
   nameDF <- AnnotationDbi::as.data.frame (org.Hs.egGENENAME)
   keyDF <- cbind (symbolDF, 'gene_name' = nameDF[, 2])
 
-  geneDF <- geneList %>%
+  geneList <- unlist(geneList)
+
+  # Return FALSE for the genes that aren't human
+  isHuman <- geneList %>%
     unlist () %>%
-    lapply (function (x) keyDF[grep (str_c ('^', x, '$'), keyDF[, 1]), 2]) %>%
-    unlist () %>%
+    map_lgl (~isTRUE(grep (str_c ('^', ., '$'), keyDF[, 1]) >= 1))
+
+  humanIDs <- geneList[c (isHuman)]
+
+  geneDF <- humanIDs %>%
+    map_chr (~keyDF[grep (str_c ('^', ., '$'), keyDF[, 1]), 2]) %>%
     as_tibble () %>%
-    dplyr::rename (gene_symbol = value) %>%
-    dplyr::count (gene_symbol) %>%
+    dplyr::rename (geneSymbol = value) %>%
+    dplyr::count (geneSymbol) %>%
     dplyr::arrange (-n) %>%
-    dplyr::mutate (gene_symbol = factor (x = gene_symbol))
+    dplyr::mutate (geneSymbol = factor (x = geneSymbol))
 
-  geneDF[, 3:4] <- NA
+  geneIDX <- map_int (geneDF[[1]], ~grep (str_c ('^', geneDF[[1]][.], '$'), keyDF[, 2]))
+  geneDF[, 3] <- as.character (keyDF[geneIDX, 3])
+  geneDF[, 4] <- as.character(tissueDF[geneIDX, 2])
 
-  for (v in 1:length (geneDF[[1]])) {
-
-    geneIDX <- grep (str_c ('^', geneDF[[1]][v], '$'), keyDF[, 2])
-    geneDF[v, 3] <- as.character (keyDF[geneIDX, 3])
-    geneDF[v, 4] <- tissueDF[geneIDX, 2]
-
-  }
+  names(geneDF) <- c('geneSymbol', 'n', 'geneName', 'tissue')
 
   return (geneDF)
 
