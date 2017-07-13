0a379126d46a0b99223d3f9bae300d06923d608f
Evan A Martin <evanamartin@gmail.com>
2017-07-13 14:00:49 -0700

    include mouse genes

--- R/cleanGenes.R
+++ R/cleanGenes.R
@@ -23,6 +23,7 @@
 #' @import dplyr
 #' @import magrittr
 #' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
 #' @import tibble
 #'
 #' @importFrom AnnotationDbi as.data.frame
@@ -32,23 +33,34 @@ cleanGenes <- function (geneList) {
 
   value <- geneSymbol <- NULL
 
-  symbolDF <- AnnotationDbi::as.data.frame (org.Hs.egSYMBOL)
-  nameDF <- AnnotationDbi::as.data.frame (org.Hs.egGENENAME)
-  keyDF <- cbind (symbolDF, 'geneName' = nameDF[, 2])
+  # Get gene symbols and names from org.Hs.eg.db
+  symbolHsDF <- AnnotationDbi::as.data.frame (org.Hs.egSYMBOL)
+  nameHsDF <- AnnotationDbi::as.data.frame (org.Hs.egGENENAME)
+  keyHsDF <- cbind (symbolHsDF, 'geneName' = nameHsDF[, 2])
+
+  # Get gene symbols and names from org.Mm.eg.db
+  symbolMmDF <- AnnotationDbi::as.data.frame (org.Mm.egSYMBOL)
+  nameMmDF <- AnnotationDbi::as.data.frame (org.Mm.egGENENAME)
+  keyMmDF <- cbind (symbolMmDF, 'geneName' = nameMmDF[, 2])
 
   # By unlisting geneList all of the NULL elements are removed
-  geneList <- unlist(geneList)
+  geneList <- unlist (geneList)
 
   # Return FALSE for the genes that aren't human because only human genes
   # are listed in the keyDF data frame
   isHuman <- geneList %>%
-    map_lgl (~isTRUE (grep (str_c ('^', ., '$'), keyDF[, 1]) >= 1))
+    map_lgl (~isTRUE (grep (str_c ('^', ., '$'), keyHsDF[, 1]) >= 1))
 
-  # Only keep the gene ids that are associated with human genes
+  isMouse <- geneList %>%
+    map_lgl (~isTRUE (grep (str_c ('^', ., '$'), keyMmDF[, 1]) >= 1))
+
+  # Separate human and mouse genes to report them in different tibbles
   humanIDs <- geneList[c (isHuman)]
+  mouseIDs <- geneList[c (isMouse)]
 
-  geneDF <- humanIDs %>%
-    map_chr (~keyDF[grep (str_c ('^', ., '$'), keyDF[, 1]), 2]) %>%
+  # Get the counts of the human genes and order them by count
+  geneHsDF <- humanIDs %>%
+    map_chr (~keyHsDF[grep (str_c ('^', ., '$'), keyHsDF[, 1]), 2]) %>%
     as_tibble () %>%
     dplyr::rename (geneSymbol = value) %>%
     dplyr::count (geneSymbol) %>%
@@ -57,13 +69,28 @@ cleanGenes <- function (geneList) {
 
   # Take only the first element returned because of duplicate gene symbols
   # in the org.Hs.egSYMBOL data base.
-  geneIDX <- geneDF[[1]] %>%
-    map_int (~grep (str_c ('^', ., '$'), keyDF[, 2])[[1]])
-  geneDF[, 3] <- as.character (keyDF[geneIDX, 3])
-  geneDF[, 4] <- as.character (tissueDF[geneIDX, 2])
+  geneIDX <- geneHsDF[[1]] %>%
+    map_int (~grep (str_c ('^', ., '$'), keyHsDF[, 2])[[1]])
+  geneHsDF[, 3] <- as.character (keyHsDF[geneIDX, 3])
+  geneHsDF[, 4] <- as.character (tissueDF[geneIDX, 2])
 
-  names(geneDF) <- c('geneSymbol', 'n', 'geneName', 'tissue')
+  names (geneHsDF) <- c ('geneSymbol', 'n', 'geneName', 'tissue')
 
-  return (geneDF)
+  # Get the counts of the mouse genes and order them by count
+  geneMmDF <- mouseIDs %>%
+    map_chr (~keyMmDF[grep (str_c ('^', ., '$'), keyMmDF[, 1]), 2]) %>%
+    as_tibble () %>%
+    dplyr::rename (geneSymbol = value) %>%
+    dplyr::count (geneSymbol) %>%
+    dplyr::arrange (-n) %>%
+    dplyr::mutate (geneSymbol = factor (x = geneSymbol))
+
+  geneMmIDX <- geneMmDF[[1]] %>%
+    map_int (~grep (str_c ('^', ., '$'), keyMmDF[, 2])[[1]])
+  geneMmDF[, 3] <- as.character (keyMmDF[geneMmIDX, 3])
+
+  names (geneMmDF) <- c ('geneSymbol', 'n', 'geneName')
+
+  return (list (human = geneHsDF, mouse = geneMmDF))
 
 }
--- tests/testthat/test-cleanGenes.R
+++ tests/testthat/test-cleanGenes.R
@@ -9,11 +9,19 @@ test_that ('A tibble with two columns is returned', {
   geneSymbols <- cleanGenes(geneids)
 
   # Check that a tibble is returned
-  expect_that (str (geneSymbols),
+  expect_that (str (geneSymbols$human),
                prints_text ('tbl_df'))
 
-  # Check that the tibble has the correct number of columns
-  expect_that (dim (geneSymbols)[2],
+  # mouse: check that a tibble is returned
+  expect_that (str (geneSymbols$mouse),
+               prints_text ('tbl_df'))
+
+  # Check that the human tibble has the correct number of columns
+  expect_that (dim (geneSymbols$human)[2],
                is_equivalent_to (4))
+
+  # Check that the mouse tibble has the correct number of columns
+  expect_that (dim (geneSymbols$mouse)[2],
+               is_equivalent_to (3))
 
 })
